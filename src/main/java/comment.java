88767084104f6a9d0ea1857582ced34f7e3b5df9
Martin Kment <st35980@student.upce.cz>
2017-12-02 11:48:41 +0100

    add regex done

--- src/main/java/comment.java
+++ src/main/java/comment.java
@@ -9,7 +9,7 @@ public class comment {
 
 
     public static void main(String[] args) throws IOException {
-        komenty();
+           komenty();
     }
 
 
@@ -20,39 +20,31 @@ public class comment {
         return name;
     }
 
+
     private static void komenty() throws IOException {
         String urlString = "https://zpravy.idnes.cz/diskuse.aspx?iddiskuse=A150730_143206_zahranicni_aba";
         Document doc = Jsoup.connect(urlString).get();
 
         String selectorContributions = "div#disc-list";
         String selectorContribution = "div.contribution";
-        //String selectorContribution = "div.msgBoxOut";
 
         Element contributions = doc.select(selectorContributions).first();
-        //Element contributions = doc.getElementById("contributions");
 
         Elements selectedDivs = contributions.select(selectorContribution);
-       /* // vypsání vybraných div
-        for (Element div : selectedDivs) {
-            System.out.println(div);
-        }
 
-        // vypsání textu vybraných div
-        for (Element div : selectedDivs) {
-            System.out.println(div.text());
-        }*/
-
-        // vyhledání elementů v rozsahu jiného elementu
         String selectorName = "h4.name";
         String selectorDate = "div.date.hover";
         String selectorContent = "div.user-text";
         for (Element div : selectedDivs) {
-            //  Element name = div.select(selectorName).first();
             Element date = div.select(selectorDate).first();
             Element content = div.select(selectorContent).first();
             String name = div.select(selectorName).first().html();
+            Document doc2 = Jsoup.parse(name);
+            Element link = doc2.select("a").first();
+            String linkHref = link.attr("href"); //
             name = regex(name);
             System.out.println(name);
+            System.out.println(linkHref);
             System.out.println(date.text());
             System.out.println(content.text());
             System.out.println("=====================================");
