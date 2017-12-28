50b220698bb8c0cd71dea047d7d257273802ad9e
Martin <st35980@student.upce.cz>
2017-12-28 17:13:35 +0100

    adding getting next page

--- src/test/groovy/CommentTest.groovy
+++ src/test/groovy/CommentTest.groovy
@@ -1,4 +1,4 @@
-import com.kment.jsoup.idnes.CommentEntity
+import com.kment.jsoup.idnes.ParseName
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
@@ -6,8 +6,16 @@ import spock.lang.Specification
 class CommentTest extends Specification {
 
     // Comment comment = new Comment()
+    ParseName parseName = new ParseName()
 
-
+    def "test parse"() {
+        when:
+        String result = parseName.regex("<!--googleoff: all--> <a href=\\\"http://muj.idnes.cz/Profil.aspx?id=1184186\\\">P<i>97</i>e<i>91</i>t<i>81</i>r <i>34</i>H<i>35</i>a<i>81</i>r<i>26</i>i<i>24</i>p<i>92</i>r<i>11</i>a<i>46</i>s<i>55</i>a<i>64</i>d <i>28</i>H<i>50</i>a<i>97</i>j<i>97</i>i<i>53</i>č</a> <sup title=\\\"Rozlišovací značka diskutujících se stejným jménem\\\">8<i>49</i>3<i>31</i>0<i>77</i>4<i>50</i>7</sup> \\n\" +\n" +
+                "                \"<!--googleon: all-->\"")
+        then:
+        result == "Petr Hariprasad Hajič 83047 \\n\" +"
+    }
+/*
     def "regex for name"() {
         when:
 
@@ -65,6 +73,6 @@ class CommentTest extends Specification {
         then:
         comment.getNumberOfPages() == 3
 
-    }
+    }*/
 
 }
