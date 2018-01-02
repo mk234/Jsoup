cd6480adc7a8e427352af2d15d49085665d119c7
Martin <st35980@student.upce.cz>
2018-01-02 18:54:13 +0100

    jUnit test

--- src/main/java/com/kment/jsoup/idnes/ParseName.java
+++ src/main/java/com/kment/jsoup/idnes/ParseName.java
@@ -3,7 +3,7 @@ package com.kment.jsoup.idnes;
 import org.jsoup.Jsoup;
 
 public class ParseName {
-    String regex(String name) {
+    public String regex(String name) {
         name = name.replaceAll("(?<=<i>).*?(?=</i>)", "");
         name = Jsoup.parse(name).text();
         name = name.substring(0, name.lastIndexOf(" "));
