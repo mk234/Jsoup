1698c2c1b669f58fc7761bf04b862fa73d9cdd25
Martin <st35980@student.upce.cz>
2018-01-07 18:41:25 +0100

    broken parser

--- src/main/java/com/kment/jsoup/idnes/Comment/ParseName.java
+++ src/main/java/com/kment/jsoup/idnes/Comment/ParseName.java
@@ -5,8 +5,17 @@ import org.jsoup.Jsoup;
 public class ParseName {
     public String regex(String name) {
         name = name.replaceAll("(?<=<i>).*?(?=</i>)", "");
+        System.out.println("1 - " + name);
         name = Jsoup.parse(name).text();
+        System.out.println("2 - " + name);
         name = name.substring(0, name.lastIndexOf(" "));
-        return name;
+        System.out.println(name.length());
+        if (name.length() == 0) {
+            System.out.println("nic tam neni");
+            return "";
+        } else {
+            System.out.println("3 - " + name);
+            return name;
+        }
     }
 }
