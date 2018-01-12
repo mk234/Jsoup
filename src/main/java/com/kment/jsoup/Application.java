b03075f98d0cf4dcfbbdac6c831d9b7f529a0134
Martin <st35980@student.upce.cz>
2018-01-12 18:04:13 +0100

    adjustment database

--- src/main/java/com/kment/jsoup/Application.java
+++ src/main/java/com/kment/jsoup/Application.java
@@ -1,22 +1,38 @@
 package com.kment.jsoup;
 
+import com.kment.jsoup.entity.Article;
+import com.kment.jsoup.idnes.IdnesRun;
+import com.kment.jsoup.springdata.IArticleSpringDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
+import java.text.ParseException;
 
 @SpringBootApplication
+public class Application implements CommandLineRunner {
 
-public class Application {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    @Autowired
+    IArticleSpringDataRepository repository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
-
+    public static void main(String[] args) throws IOException, ParseException {
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        IdnesRun idnesRun = applicationContext.getBean(IdnesRun.class);
+        idnesRun.run();
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("All article -> {}", repository.save(new Article("name", "url", "created", "kastCollection", "keywords")));
+        logger.info("All article -> {}", repository.save(new Article("name2", "url", "created", "kastCollection", "keywords")));
+        logger.info("All article -> {}", repository.findAll());
 
-
+    }
 }
