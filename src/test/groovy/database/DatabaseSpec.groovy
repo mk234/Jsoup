3f8d0a6af4b83ed9802a0f1d91245887f6ebe1ae
Martin <st35980@student.upce.cz>
2018-04-15 17:18:12 +0200

    fix dateFormat to cze

--- src/test/groovy/database/DatabaseSpec.groovy
+++ src/test/groovy/database/DatabaseSpec.groovy
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.Rollback
 import org.springframework.transaction.annotation.Transactional
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import java.text.SimpleDateFormat
 
+@Ignore
 @Transactional
 @SpringBootTest(classes = Application.class)
 class DatabaseSpec extends Specification {
--- src/test/groovy/portals/ExtractArticlesFromArchiveSpec.groovy
+++ src/test/groovy/portals/ExtractArticlesFromArchiveSpec.groovy
@@ -11,11 +11,9 @@ import novinky.source.ExtractArticlesNovinkyPreparedData
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.stereotype.Repository
-import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
 
-@Ignore
 @SpringBootTest(classes = Application.class)
 @Repository
 class ExtractArticlesFromArchiveSpec extends Specification {
