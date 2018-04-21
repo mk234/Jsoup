046a1dda404b165c6f6228961b68fa6969d5ef7a
Martin <st35980@student.upce.cz>
2018-04-21 11:50:10 +0200

    fix locale

--- src/main/java/com/kment/jsoup/Application.java
+++ src/main/java/com/kment/jsoup/Application.java
@@ -25,6 +25,7 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... strings) {
         scheduledTasks.scheduledRun();
+
 //        init.initPortals();
     }
 
