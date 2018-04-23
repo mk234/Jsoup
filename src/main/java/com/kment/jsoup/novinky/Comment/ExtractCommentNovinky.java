1bbf4284d94b0f1cb93055a3650e3197486ec58f
Martin <st35980@student.upce.cz>
2018-04-23 19:12:15 +0200

    change date format

--- src/main/java/com/kment/jsoup/novinky/Comment/ExtractCommentNovinky.java
+++ src/main/java/com/kment/jsoup/novinky/Comment/ExtractCommentNovinky.java
@@ -3,6 +3,7 @@ package com.kment.jsoup.novinky.Comment;
 import com.kment.jsoup.entity.Comment;
 import com.kment.jsoup.extractor.ParseUrl;
 import com.kment.jsoup.novinky.NumberOfPagesNovinky;
+import org.apache.commons.lang3.time.DateUtils;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -10,10 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 
 @Component
@@ -80,18 +78,45 @@ public class ExtractCommentNovinky {
     //nutna zmena formatu
     public Date getCreatedDate(Element dateElement) throws ParseException {
         String data = dateElement.text();
+        Date date;
         data = data.substring(data.indexOf(",") + 2, data.length());
         if (data.contains(":")) {
-            System.out.println(data);
-            SimpleDateFormat sdf = new SimpleDateFormat("dd. MMM yyyy, HH:mm:ss");
-            Date date = sdf.parse(data);
+            String month = data.substring(data.indexOf(".") + 2, data.lastIndexOf(",") - 5);
+            String day = data.substring(0, data.lastIndexOf("."));
+            String year = data.substring(data.lastIndexOf(",") - 4, data.lastIndexOf(","));
+            String time = data.substring(data.indexOf(",") + 2, data.length());
+            date = DateUtils.parseDateStrictly(day + "." + month + " " + year + ", " + time, "dd.MMM yyyy, HH:mm:ss");
             return date;
         } else {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd. MMM yyyy");
-            Date date = sdf.parse(data);
+            String month = data.substring(data.indexOf(".") + 2, data.length() - 5);
+            String day = data.substring(0, data.lastIndexOf("."));
+            String year = data.substring(data.length() - 4, data.length());
+            date = DateUtils.parseDateStrictly(day + "." + month + " " + year + ", 00:00", "dd.MMM yyyy, HH:mm");
             return date;
         }
+
     }
 
 
+    public int getMonth(String month) {
+        Map<String, Integer> months = fillMapWithMonths();
+        return months.get(month);
+    }
+
+    private Map<String, Integer> fillMapWithMonths() {
+        Map<String, Integer> months = new HashMap<String, Integer>();
+        months.put("ledna", 0);
+        months.put("února", 1);
+        months.put("března", 2);
+        months.put("dubna", 3);
+        months.put("května", 4);
+        months.put("června", 5);
+        months.put("července", 6);
+        months.put("srpna", 7);
+        months.put("září", 8);
+        months.put("října", 9);
+        months.put("listopadu", 10);
+        months.put("prosince", 11);
+        return months;
+    }
 }
