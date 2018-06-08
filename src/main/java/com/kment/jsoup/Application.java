6a0a14427b5ab164dc72ed4eaa94aa362c786ff7
mk234 <st35980@student.upce.cz>
2018-06-08 11:17:34 +0200

    Update Application.java

--- src/main/java/com/kment/jsoup/Application.java
+++ src/main/java/com/kment/jsoup/Application.java
@@ -24,7 +24,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-        init.initPortals();
+      //  init.initPortals();
     }
 
 
